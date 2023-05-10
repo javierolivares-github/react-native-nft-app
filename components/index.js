16b7f05af3f63ad02c051aa451f9b5f2bd6d336f
Francisco Javier Olivares Diaz <franciscojavier.olivaresd@gmail.com>
2023-05-10 18:23:28 -0400

    done

---
+++ components/index.js
@@ -0,0 +1,13 @@
+import HomeHeader from './HomeHeader';
+import FocusedStatusBar from './FocusedStatusBar';
+import NFTCard from './NFTCard';
+import DetailsDesc from './DetailsDesc';
+import DetailsBid from './DetailsBid';
+
+export {
+  HomeHeader,
+  FocusedStatusBar,
+  NFTCard,
+  DetailsDesc,
+  DetailsBid
+}
